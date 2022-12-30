0d9a7f5e7b909329434264f24ada324cf8a5679a
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-12-30 12:38:29 -0600

    same behaviour with context consumer...

---
+++ contextapp/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+import { AuthContext } from '../context/AuthContext';
+
+class Navbar extends Component {
+
+    // static contextType = ThemeContext
+
+
+
+    render() { 
+
+        // console.log(this.context);
+        // const { isLightTheme, light, dark } = this.context
+        // const theme = isLightTheme ? light : dark
+
+        return (        
+
+            <AuthContext.Consumer>{(authContext) => (
+                
+                <ThemeContext.Consumer>{(themeContext) => {
+                    
+                    const { isAuthenticated, toggleAuth } = authContext
+
+                    const { isLightTheme, light, dark } = themeContext
+                    const theme = isLightTheme ? light : dark
+                    
+                    return( 
+                        <nav style={{backgroundColor: theme.ui, color: theme.syntax}}>
+                        <h1>Context App</h1>
+
+                        <div onClick={toggleAuth} style={{cursor: 'pointer'}}>
+                            {isAuthenticated ? "Logged in" : "Logged out"}
+                        </div>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                    )
+                }}
+                </ThemeContext.Consumer>
+            )}
+            </AuthContext.Consumer>
+
+        );
+    }
+}
+ 
+export default Navbar;
